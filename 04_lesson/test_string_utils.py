4ed4a2df6fa90ae895da74ad2f3711a5c4458d2f
feniks07180 <Niki1-02@mail.ru>
2025-05-24 19:11:46 +0400

    1.0.0.0

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -7,31 +7,43 @@ def string_utils():
 
 class TestStringUtils:
 
-    def test_capitalize(self, string_utils):
-        assert string_utils.capitalize("skypro") == "Skypro"
-        assert string_utils.capitalize("Skypro") == "Skypro"
-        assert string_utils.capitalize("") == ""
-        assert string_utils.capitalize("123") == "123"
+    class TestCapitalize:
+        def test_capitalize_positive(self, string_utils):
+            assert string_utils.capitalize("skypro") == "Skypro"
+            assert string_utils.capitalize("Skypro") == "Skypro"
+            assert string_utils.capitalize("123") == "123"
 
-    def test_trim(self, string_utils):
-        assert string_utils.trim("   skypro") == "skypro"
-        assert string_utils.trim("skypro   ") == "skypro   "
-        assert string_utils.trim("  skypro  ") == "skypro  "
-        assert string_utils.trim("skypro") == "skypro"
-        assert string_utils.trim("") == ""
-        assert string_utils.trim("   ") == ""
+        def test_capitalize_negative(self, string_utils):
+            assert string_utils.capitalize("") == ""
 
-    def test_contains(self, string_utils):
-        assert string_utils.contains("SkyPro", "S") == True
-        assert string_utils.contains("SkyPro", "U") == False
-        assert string_utils.contains("SkyPro", "k") == True
-        assert string_utils.contains("", "a") == False
-        assert string_utils.contains("abc", "") == True
+    class TestTrim:
+        def test_trim_positive(self, string_utils):
+            assert string_utils.trim("   skypro") == "skypro"
+            assert string_utils.trim("skypro   ") == "skypro   "
+            assert string_utils.trim("  skypro  ") == "skypro  "
+            assert string_utils.trim("skypro") == "skypro"
 
-    def test_delete_symbol(self, string_utils):
-        assert string_utils.delete_symbol("SkyPro", "k") == "SyPro"
-        assert string_utils.delete_symbol("SkyPro", "Pro") == "Sky"
-        assert string_utils.delete_symbol("SkyPro", "z") == "SkyPro"
-        assert string_utils.delete_symbol("aaaa", "a") == ""
-        assert string_utils.delete_symbol("", "a") == ""
-        assert string_utils.delete_symbol("abc", "") == "abc"
+        def test_trim_negative(self, string_utils):
+            assert string_utils.trim("") == ""
+            assert string_utils.trim("   ") == ""
+
+    class TestContains:
+        def test_contains_positive(self, string_utils):
+            assert string_utils.contains("SkyPro", "S") == True
+            assert string_utils.contains("SkyPro", "k") == True
+            assert string_utils.contains("abc", "") == True
+
+        def test_contains_negative(self, string_utils):
+            assert string_utils.contains("SkyPro", "U") == False
+            assert string_utils.contains("", "a") == False
+
+    class TestDeleteSymbol:
+        def test_delete_symbol_positive(self, string_utils):
+            assert string_utils.delete_symbol("SkyPro", "k") == "SyPro"
+            assert string_utils.delete_symbol("SkyPro", "Pro") == "Sky"
+            assert string_utils.delete_symbol("aaaa", "a") == ""
+            assert string_utils.delete_symbol("abc", "") == "abc"
+
+        def test_delete_symbol_negative(self, string_utils):
+            assert string_utils.delete_symbol("SkyPro", "z") == "SkyPro"
+            assert string_utils.delete_symbol("", "a") == ""
